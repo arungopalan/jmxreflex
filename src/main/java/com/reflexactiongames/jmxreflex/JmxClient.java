7cb556a4ca67adac87d14f997624d0047c49bcfc
U-livingroom-pc\rcbose <rcbose@livingroom-pc.(none)>
2013-10-12 12:33:45 -0400

    Adding first draft

---
+++ src/main/java/com/reflexactiongames/jmxreflex/JmxClient.java
@@ -0,0 +1,123 @@
+package com.reflexactiongames.jmxreflex;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+
+public class JmxClient {
+
+	private static final String CONNECTOR_ADDRESS = "com.sun.management.jmxremote.localConnectorAddress";
+
+	private JmxClient() {
+		// All methods are static!
+	}
+
+	public static void invokeByPid(String pid, String beanName, String methodName, Object[] params) throws Exception {
+		invokeByMainClassOrPid(null, pid, beanName, methodName, params);
+	}
+
+	public static void invokeByUrl(String url1, String beanName, String methodName, Object[] params) throws Exception {
+		JMXServiceURL jmxServiceUrl = new JMXServiceURL(url1);
+		JMXConnector connector = null;
+
+		try {
+			connector = JMXConnectorFactory.connect(jmxServiceUrl);
+			MBeanServerConnection mbeanConn = connector.getMBeanServerConnection();
+			Set<ObjectName> beanSet = mbeanConn.queryNames(null, null);
+			System.out.println(beanSet);
+			ObjectName mbeanName = new ObjectName(beanName);
+			mbeanConn.invoke(mbeanName, methodName, params, null);
+
+		} finally {
+			if (connector != null) {
+				try {
+					connector.close();
+				} catch (Exception e) {
+
+				}
+			}
+		}
+	}
+
+	public static void invokeByMainClass(String mainClassName, String beanName, String methodName, Object[] params)
+			throws Exception {
+		invokeByMainClassOrPid(mainClassName, null, beanName, methodName, params);
+	}
+
+	private static void invokeByMainClassOrPid(String mainClassName, String pid, String beanName, String methodName,
+			Object[] params) throws Exception {
+
+		if (mainClassName == null && pid == null) {
+			throw new RuntimeException("Either main class or pid must be passed.");
+		}
+
+		if (mainClassName != null && pid != null) {
+			throw new RuntimeException("Only one of mainClassName or pid should be passed.");
+		}
+
+		List<VirtualMachineDescriptor> virtualMachineDescriptors = VirtualMachine.list();
+
+		boolean serverFound = false;
+
+		for (VirtualMachineDescriptor desc : virtualMachineDescriptors) {
+
+			if (desc.displayName().equals(mainClassName) || desc.id().equals(pid)) {
+
+				serverFound = true;
+
+				VirtualMachine virtualMachine = VirtualMachine.attach(desc);
+				Properties props = virtualMachine.getAgentProperties();
+				String connectorAddress = props.getProperty(CONNECTOR_ADDRESS);
+
+				// no connector address, so we start the JMX agent
+				if (connectorAddress == null) {
+					String agent = virtualMachine.getSystemProperties().getProperty("java.home") + File.separator
+							+ "lib" + File.separator + "management-agent.jar";
+					virtualMachine.loadAgent(agent);
+
+					// agent is started, get the connector address
+					connectorAddress = virtualMachine.getAgentProperties().getProperty(CONNECTOR_ADDRESS);
+					assert connectorAddress != null;
+				}
+
+				JMXServiceURL url = new JMXServiceURL(connectorAddress);
+				JMXConnector connector = null;
+
+				try {
+					connector = JMXConnectorFactory.connect(url);
+					MBeanServerConnection mbeanConn = connector.getMBeanServerConnection();
+					Set<ObjectName> beanSet = mbeanConn.queryNames(null, null);
+					System.out.println(beanSet);
+					ObjectName mbeanName = new ObjectName(beanName);
+					mbeanConn.invoke(mbeanName, methodName, params, null);
+
+				} finally {
+					if (connector != null) {
+						try {
+							connector.close();
+						} catch (Exception e) {
+
+						}
+					}
+				}
+			}
+
+		}
+
+		if (!serverFound) {
+			throw new Exception("No server found");
+		}
+
+	}
+
+}
---
+++ src/main/java/com/reflexactiongames/jmxreflex/Main.java
@@ -0,0 +1,116 @@
+package com.reflexactiongames.jmxreflex;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
+public class Main {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+
+		// create Options object
+		Options options = new Options();
+		Option urlOption = OptionBuilder.withArgName("u").hasArg().withDescription("The JMX URL to connect to")
+				.create("u");
+		options.addOption(urlOption);
+
+		Option processIdOption = OptionBuilder
+				.withArgName("p")
+				.hasArg()
+				.withDescription(
+						"The process id of the Java process to connect to. "
+								+ "This can be done only if the java process is running on the same machine and was started by the same user. "
+								+ "See JMX documentation for more detail.").create("p");
+		options.addOption(processIdOption);
+
+		Option mainClassOption = OptionBuilder.withArgName("c").hasArg().withDescription("The name of the main class")
+				.create("c");
+		options.addOption(mainClassOption);
+
+		Option mbeanNameOption = OptionBuilder.withArgName("b").hasArg().withDescription("The name of the mbean")
+				.create("b");
+		options.addOption(mbeanNameOption);
+
+		Option methodNameOption = OptionBuilder.withArgName("m").hasArg()
+				.withDescription("The method to invoke on the mbean").create("m");
+		options.addOption(methodNameOption);
+
+		Option argumentsOption = OptionBuilder
+				.withArgName("a")
+				.hasArg()
+				.withDescription(
+						"A comma separated value of arguments to pass to the method. Only string values are supported")
+				.create("a");
+		options.addOption(argumentsOption);
+
+		CommandLineParser parser = new BasicParser();
+
+		CommandLine commandLine = parser.parse(options, args);
+
+		String url = null, pid = null, mainClassName = null, beanName = null, methodName = null;
+
+		int connectionParams = 0;
+
+		if (commandLine.hasOption("u")) {
+			url = commandLine.getOptionValue("u");
+			connectionParams++;
+		}
+
+		if (commandLine.hasOption("p")) {
+			pid = commandLine.getOptionValue("p");
+			connectionParams++;
+		}
+
+		if (commandLine.hasOption("c")) {
+			mainClassName = commandLine.getOptionValue("c");
+			connectionParams++;
+		}
+
+		if (connectionParams == 0) {
+			System.err.print("You must pass either URL, PID or Main class");
+			System.exit(1);
+		}
+
+		if (connectionParams > 1) {
+			System.err.print("Please pass only one option between URL, PID or Main class name.");
+			System.exit(1);
+		}
+
+		if (commandLine.hasOption("b")) {
+			beanName = commandLine.getOptionValue("b");
+		} else {
+			System.err.print("Bean name is required");
+			System.exit(1);
+		}
+
+		if (commandLine.hasOption("m")) {
+			beanName = commandLine.getOptionValue("m");
+		} else {
+			System.err.print("Method name is required");
+			System.exit(1);
+		}
+
+		Object[] params = null;
+		if (commandLine.hasOption("a")) {
+			String argumentsCsv = commandLine.getOptionValue("a");
+			String[] arguments = argumentsCsv.split(",");
+			params = new Object[arguments.length];
+			int index = 0;
+			for (String argument : arguments) {
+				params[index] = argument;
+			}
+		}
+
+		if (mainClassName != null) {
+			JmxClient.invokeByMainClass(mainClassName, beanName, methodName, params);
+		} else if (pid != null) {
+			JmxClient.invokeByPid(pid, beanName, methodName, params);
+		} else {
+			JmxClient.invokeByUrl(url, beanName, methodName, params);
+		}
+	}
+}
